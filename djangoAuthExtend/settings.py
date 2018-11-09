4e4c912c0615c77f938467cb3cf795f8288eec38
yeongwave <playinlion@likelion.org>
2018-11-09 21:12:52 +0900

    Custom userModel
    AbstractUser 상속
    UserCreationForm, UserChangeForm 커스텀

--- djangoAuthExtend/settings.py
+++ djangoAuthExtend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'users.CustomUser'
